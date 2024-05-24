dcdea5563928314a04878895cc437f99fb381c08
Justin <122790149+jquagga@users.noreply.github.com>
2024-05-24 07:45:40 -0400

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -42,9 +42,6 @@ def transcribe_whisper(calljson, audiofile):
         vad_filter=vad_filter,
         vad_parameters=dict(min_silence_duration_ms=500),
         language=language,
-        # This enhances distil models but is not required for "normal"
-        # Without it, distil models are bonkers.
-        condition_on_previous_text=False,
     )
 
     calltext = "".join(segment.text for segment in segments)
@@ -265,11 +262,7 @@ def main():
             else:
                 calljson = transcribe_whisper(calljson, audiofile)
 
-            # When Whisper process a file with no speech, it tends to spit out "you"
-            # Just "you" and nothing else.
-            # So if the transcript is just "you", don't bother sending the notification,
-            # we will just delete the files and keep going to the next call.
-            if calljson["text"].strip() != "you":
+            if calljson["text"]:
                 send_notifications(calljson, audiofile, destinations)
 
             # And now delete the files from the transcribe directory
